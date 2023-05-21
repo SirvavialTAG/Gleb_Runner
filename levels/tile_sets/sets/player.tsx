c0d78af1d9bf92e0848dc8fb7deb72d5215faf1d
SirvavialTAG <123053453+SirvavialTAG@users.noreply.github.com>
2023-05-21 18:59:55 +1000

    Add files via upload

---
+++ levels/tile_sets/sets/player.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="player" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../../../../Telegram/старт.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../../../../Telegram/end.png"/>
+ </tile>
+</tileset>

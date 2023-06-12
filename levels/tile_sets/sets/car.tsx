9516c39d7e8d385edaa8fa0886283e92515c298d
SirvavialTAG <123053453+SirvavialTAG@users.noreply.github.com>
2023-06-12 23:49:36 +1000

    Add files via upload

---
+++ levels/tile_sets/sets/car.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="car" tilewidth="64" tileheight="64" tilecount="8" columns="4">
+ <image source="../../../images/car.png" width="256" height="128"/>
+</tileset>
---
+++ levels/tile_sets/sets/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="3c241b49fec9e9f0" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../images/3c241b49fec9e9f0.png" width="64" height="64"/>
+</tileset>
